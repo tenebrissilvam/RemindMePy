2ffc0df215de8b4a72ffcfc2286a83c9b76ab6fd
tenebrissilvam <bakaeva.ea@phystech.edu>
2023-04-30 03:44:07 +0300

    finish README.md

--- handlers/basic_commands.py
+++ handlers/basic_commands.py
@@ -10,5 +10,5 @@ async def cmd_start(message: types.Message):
 async def cmd_help(message: types.Message):
     await message.answer(
         "Доступные команды бота:\n /add_reminder : Создание нового напоминания,\n /edit_reminder {id}: Изменение напоминания"
-        "по его id, \n /delete_reminder {id}: удаление напоминания по его id, \n /list_all : вывод всех напоминаний с их id."
+        " по его id, \n /delete_reminder {id}: удаление напоминания по его id, \n /list_all : вывод всех напоминаний с их id."
     )
